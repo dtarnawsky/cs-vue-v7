baf720020602b2661f89e63dfb5dbe30d8e1cdaa
dtarnawsky <damian@ionic.io>
2023-05-02 12:57:59 -0700

    initial

---
+++ android/app/src/main/java/cs/vue/v7/MainActivity.java
@@ -0,0 +1,5 @@
+package cs.vue.v7;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.vue.v7',
+  appName: 'cs-vue-v7',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
